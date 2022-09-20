6859f2c6b869b8bc1978a0faf52686ffa1ce2f8b
Dirk Kretschmann <d.kretschmann@dotsource.de>
2022-09-20 22:21:39 +0200

    remove ScreenPoint type

--- src/common.rs
+++ src/common.rs
@@ -7,20 +7,15 @@ use std::ops::Sub;
 pub const TILE_SIZE: Point = Point::new(3, 1);
 pub const FRAMES_PER_SECOND: u16 = 8;
 
-pub type Point = Point2;
-
 #[derive(Clone, Eq, Hash, Ord, PartialEq, PartialOrd)]
-pub struct Point2 {
+pub struct Point {
     pub x: i32,
     pub y: i32,
 }
 
-impl Point2 {
+impl Point {
     pub const fn new(x: i32, y: i32) -> Self {
-        Self {
-            x,
-            y,
-        }
+        Self { x, y }
     }
 
     pub const fn width(&self) -> i32 {
@@ -48,38 +43,38 @@ impl Point2 {
     }
 }
 
-impl std::fmt::Debug for Point2 {
+impl std::fmt::Debug for Point {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{{ x: {}, y: {} }}", self.x, self.y)
     }
 }
 
-impl Add for Point2 {
-    type Output = Point2;
+impl Add for Point {
+    type Output = Point;
 
     fn add(self, rhs: Self) -> Self::Output {
         Self::Output::new(self.x + rhs.x, self.y + rhs.y)
     }
 }
 
-impl Add for &Point2 {
-    type Output = Point2;
+impl Add for &Point {
+    type Output = Point;
 
     fn add(self, rhs: Self) -> Self::Output {
         Self::Output::new(self.x + rhs.x, self.y + rhs.y)
     }
 }
 
-impl Sub for Point2 {
-    type Output = Point2;
+impl Sub for Point {
+    type Output = Point;
 
     fn sub(self, rhs: Self) -> Self::Output {
         Self::Output::new(self.x - rhs.x, self.y - rhs.y)
     }
 }
 
-impl Sub for &Point2 {
-    type Output = Point2;
+impl Sub for &Point {
+    type Output = Point;
 
     fn sub(self, rhs: Self) -> Self::Output {
         Self::Output::new(self.x - rhs.x, self.y - rhs.y)
